bb2d8b95efb3d2866bd110d381f85b5ba40003cd
Igor <belogurovis@gmail.com>
2017-10-15 23:10:51 +0300

    First commit

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Books);
+        }
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            return View(id);
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+    }
+}
